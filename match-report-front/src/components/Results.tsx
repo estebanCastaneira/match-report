f833cd6feece9fb7b3f562cdf1b195d643f2c67e
estebanCastaneira <estebanc_b@hotmail.com>
2023-09-09 15:59:17 -0300

    Stat WIP

--- match-report-front/src/components/Results.tsx
+++ match-report-front/src/components/Results.tsx
@@ -4,16 +4,16 @@ import TeamAway from "./TeamAway";
 import Stat from "./Stat";
 function Results(): JSX.Element {
   return (
-    <>
-      <div className="flex justify-around border-b py-4 mx-4 border-b-neutral-900">
+    <div className="mx-4">
+      <div className="flex justify-around border-b py-4  border-b-neutral-900">
         <TeamHome />
         <Score />
         <TeamAway />
       </div>
-      <div>
+      <div className="mt-5 mx-5">
         <Stat />
       </div>
-    </>
+    </div>
   );
 }
 
--- match-report-front/src/components/Stat.tsx
+++ match-report-front/src/components/Stat.tsx
@@ -1,5 +1,14 @@
 function Stat(): JSX.Element {
-  return <></>;
+  return (
+    <div>
+      <div className="flex justify-between items-center">
+        <p>10</p>
+        <h4 className="uppercase text-stone-500 text-sm">Stat Name</h4>
+        <p>4</p>
+      </div>
+      <div className="w-full"></div>
+    </div>
+  );
 }
 
 export default Stat;

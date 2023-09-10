25eacfb63cadfb31f29ce25ec15b19c0e5b74271
estebanCastaneira <estebanc_b@hotmail.com>
2023-09-10 11:08:37 -0300

    LineUp WIP

--- match-report-front/src/components/LineupList.tsx
+++ match-report-front/src/components/LineupList.tsx
@@ -1,27 +1,52 @@
 import Player from "./Player";
+import condition from "../assets/icons/Condition 5.png";
 
 function LineupList(): JSX.Element {
   return (
-    <div>
-      <div className="bg-neutral-900 border border-neutral-800 p-2 table w-full">
-        <div className="table-header-group">
-          <div className="table-cell">
-            <p>Lineup</p>
-          </div>
-          <div className="table-cell">
-            <p>Player</p>
-          </div>
-          <div className="table-cell">
-            <p>Ray</p>
-          </div>
-          <div className="table-cell">
-            <p>Rating</p>
-          </div>
-        </div>
-      </div>
+    <div className="relative overflow-x-auto">
+      <table className="w-full text-left border border-neutral-900 text-white">
+        <thead className="bg-neutral-900 ">
+          <tr className="border border-neutral-800">
+            <th scope="col" className="px-6 py-3">
+              Lineup
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Player
+            </th>
+            <th scope="col" className="px-6 py-3">
+              <img src={condition} alt="Player condition icon" />
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Rating
+            </th>
+          </tr>
+        </thead>
+      </table>
       <Player />
     </div>
   );
 }
 
 export default LineupList;
+
+{
+  /* <div>
+<div className="border border-neutral-800 table m-2 w-full">
+  <div className="table-header-group bg-neutral-900 p-4 m-2">
+    <div className="table-cell">
+      <p>Lineup</p>
+    </div>
+    <div className="table-cell">
+      <p>Player</p>
+    </div>
+    <div className="table-cell">
+      <img src={condition} className="condition-icon" />
+    </div>
+    <div className="table-cell">
+      <p>Rating</p>
+    </div>
+  </div>
+  <Player />
+</div>
+</div> */
+}
--- match-report-front/src/components/Player.tsx
+++ match-report-front/src/components/Player.tsx
@@ -1,5 +1,14 @@
 function Player(): JSX.Element {
-  return <></>;
+  return (
+    <tbody>
+      <tr className="">
+        <td className="px-6 py-4 text-white">Apple MacBook Pro 17"</td>
+        <td className="px-6 py-4 text-white">Silver</td>
+        <td className="px-6 py-4 text-white">Laptop</td>
+        <td className="px-6 py-4 text-white">$2999</td>
+      </tr>
+    </tbody>
+  );
 }
 
 export default Player;

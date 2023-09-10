87b6c46d1c27dc5ed105f41703aca7ba5bd43921
estebanCastaneira <estebanc_b@hotmail.com>
2023-09-10 11:31:29 -0300

    Lineup wip

--- match-report-front/src/components/LineupList.tsx
+++ match-report-front/src/components/LineupList.tsx
@@ -1,28 +1,31 @@
 import Player from "./Player";
-import condition from "../assets/icons/Condition 5.png";
+import condition from "../assets/icons/condition.png";
 
 function LineupList(): JSX.Element {
   return (
     <div className="relative overflow-x-auto">
       <table className="w-full text-left border border-neutral-900 text-white">
-        <thead className="bg-neutral-900 ">
+        <thead className="bg-neutral-900">
           <tr className="border border-neutral-800">
-            <th scope="col" className="px-6 py-3">
-              Lineup
+            <th scope="col" className="px-6 py-3 border-r border-neutral-800">
+              <p className="font-light">Lineup</p>
             </th>
-            <th scope="col" className="px-6 py-3">
-              Player
+            <th scope="col" className="px-6 py-3 border-r border-neutral-800">
+              <p className="font-light">Player</p>
             </th>
-            <th scope="col" className="px-6 py-3">
+            <th
+              scope="col"
+              className="px-6 py-3 border-r border-neutral-800 flex items-center justify-center"
+            >
               <img src={condition} alt="Player condition icon" />
             </th>
             <th scope="col" className="px-6 py-3">
-              Rating
+              <p className="font-light">Rating</p>
             </th>
           </tr>
         </thead>
+        <Player />
       </table>
-      <Player />
     </div>
   );
 }
--- match-report-front/src/components/Player.tsx
+++ match-report-front/src/components/Player.tsx
@@ -1,11 +1,21 @@
+import fooball from "../assets/icons/football.svg";
 function Player(): JSX.Element {
   return (
     <tbody>
-      <tr className="">
-        <td className="px-6 py-4 text-white">Apple MacBook Pro 17"</td>
-        <td className="px-6 py-4 text-white">Silver</td>
-        <td className="px-6 py-4 text-white">Laptop</td>
-        <td className="px-6 py-4 text-white">$2999</td>
+      <tr className="font-bold">
+        <td scope="row" className="px-6 py-3 border-r border-neutral-800">
+          <p className="inline">1</p> <p className="inline uppercase">gk</p>
+        </td>
+        <td className="px-6 py-3 border-r border-neutral-800 flex justify-between">
+          <p className="inline uppercase">P.Surname</p>
+          <img width={20} height={10} src={fooball} alt="football" />
+        </td>
+        <td className="px-6 py-3 border-r border-neutral-800">
+          <p className="inline">96</p>
+        </td>
+        <td className="px-6 py-3">
+          <p className="inline">8.4</p>
+        </td>
       </tr>
     </tbody>
   );

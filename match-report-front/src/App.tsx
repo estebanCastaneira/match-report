756858799a188a9ba78bdcffe705fc1573bd512f
estebanCastaneira <estebanc_b@hotmail.com>
2023-09-12 13:15:06 -0300

    Zones pages axios call get score & attackzones

--- match-report-front/src/App.tsx
+++ match-report-front/src/App.tsx
@@ -1,52 +1,27 @@
 import Stats from "./pages/Stats";
 import Lineup from "./pages/Lineup";
+import Zones from "./pages/Zones";
 import Navbar from "./components/Navbar";
-import Attack from "./components/Attack";
-import axios from "axios";
-import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import createStats from "./functions/createStats.js";
 import "tailwindcss/tailwind.css";
 import "./App.css";
 import "./assets/styles/fonts.css";
 
 function App(): JSX.Element {
-  const [stats, setStats] = useState();
-  useEffect((): void => {
-    const fetchData = async (): Promise<void> => {
-      try {
-        const [getStats] = await Promise.all([
-          axios.get(`${import.meta.env.VITE_BACK_URL}/stats`),
-        ]);
-        const allStats = {
-          leftTeam: getStats.data.leftTeam,
-          rightTeam: getStats.data.rightTeam,
-        };
-        setStats(createStats(allStats));
-      } catch (error) {
-        console.log("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
-    stats && (
-      <>
-        <main className="my-10  mx-auto max-w-4xl">
-          <h1 className="m-5 font-light">Match Report</h1>
-          <Navbar />
-          <div className="mx-2">
-            <Routes>
-              <Route path="/" element={<Stats />} />
-              <Route path="/lineup" element={<Lineup />} />
-              <Route path="/zones" element={<Attack stats={stats} />} />
-            </Routes>
-          </div>
-        </main>
-      </>
-    )
+    <>
+      <main className="my-10  mx-auto max-w-4xl">
+        <h1 className="m-5 font-light">Match Report</h1>
+        <Navbar />
+        <div className="mx-2">
+          <Routes>
+            <Route path="/" element={<Stats />} />
+            <Route path="/lineup" element={<Lineup />} />
+            <Route path="/zones" element={<Zones />} />
+          </Routes>
+        </div>
+      </main>
+    </>
   );
 }
 
--- match-report-front/src/pages/Zones.tsx
+++ match-report-front/src/pages/Zones.tsx
@@ -1,7 +1,39 @@
 import Attack from "../components/Attack";
+import { useState, useEffect } from "react";
+import createStats from "../functions/createStats";
+import axios from "axios";
 
 function Zones(): JSX.Element {
-  return <Attack />;
+  const [stats, setStats] = useState();
+  const [score, setScore] = useState();
+  useEffect((): void => {
+    const fetchData = async (): Promise<void> => {
+      try {
+        const [getStats, getMatch] = await Promise.all([
+          axios.get(`${import.meta.env.VITE_BACK_URL}/stats`),
+          axios.get(`${import.meta.env.VITE_BACK_URL}/match`),
+        ]);
+        const allStats = {
+          leftTeam: getStats.data.leftTeam,
+          rightTeam: getStats.data.rightTeam,
+        };
+        setStats(createStats(allStats));
+        setScore(getMatch.data.score);
+      } catch (error) {
+        console.log("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  return (
+    stats && (
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="uppercase text-neutral-500">Attack Zones</h1>
+        <Attack stats={stats} />
+      </div>
+    )
+  );
 }
 
 export default Zones;

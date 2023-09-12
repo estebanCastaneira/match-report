f950ea299d3304bc158993e38e778d29b34795be
estebanCastaneira <estebanc_b@hotmail.com>
2023-09-12 13:19:28 -0300

    Zones with score & teams

--- match-report-front/src/pages/Zones.tsx
+++ match-report-front/src/pages/Zones.tsx
@@ -1,4 +1,7 @@
 import Attack from "../components/Attack";
+import LeftTeam from "../components/LeftTeam";
+import RightTeam from "../components/RightTeam";
+import Score from "../components/Score";
 import { useState, useEffect } from "react";
 import createStats from "../functions/createStats";
 import axios from "axios";
@@ -26,10 +29,19 @@ function Zones(): JSX.Element {
 
     fetchData();
   }, []);
+
   return (
-    stats && (
+    stats &&
+    score && (
       <div className="flex flex-col justify-center items-center">
-        <h1 className="uppercase text-neutral-500">Attack Zones</h1>
+        <div className="flex justify-around border-b py-4  border-b-neutral-900 md:justify-center md:gap-7 md:mx-auto">
+          <LeftTeam />
+          <Score score={score} />
+          <RightTeam />
+        </div>
+        <h1 className="text-xs text-neutral-500 uppercase my-5">
+          Attack Zones
+        </h1>
         <Attack stats={stats} />
       </div>
     )
